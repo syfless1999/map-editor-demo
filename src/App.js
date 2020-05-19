c5479f2c7571cc26fce56dc86c5b982c876ecf5f
syfless <1062525195@qq.com>
2020-05-19 11:33:59 +0800

    eject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GGEditor, { Flow, Item, ItemPanel } from 'gg-editor';
+import styles from './index.less';
+
+const data = {
+  nodes: [],
+  edges: [],
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <GGEditor>
+      <ItemPanel className={styles.itemPanel}>
+        <Item
+          className={styles.item}
+          model={{
+            type: 'circle',
+            size: 50,
+            label: 'circle',
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <img
+            src="https://gw.alicdn.com/tfs/TB1IRuSnRr0gK0jSZFnXXbRRXXa-110-112.png"
+            width="55"
+            height="56"
+            draggable={false}
+          />
+        </Item>
+        <Item
+          className={styles.item}
+          model={{
+            type: 'rect',
+            size: [80, 24],
+            label: 'rect',
+          }}
+        >
+          <img
+            src="https://gw.alicdn.com/tfs/TB1reKOnUT1gK0jSZFrXXcNCXXa-178-76.png"
+            width="89"
+            height="38"
+            draggable={false}
+          />
+        </Item>
+        <Item
+          className={styles.item}
+          model={{
+            type: 'ellipse',
+            size: [100, 50],
+            label: 'ellipse',
+          }}
+        >
+          <img
+            src="https://gw.alicdn.com/tfs/TB1AvmVnUH1gK0jSZSyXXXtlpXa-216-126.png"
+            width="108"
+            height="63"
+            draggable={false}
+          />
+        </Item>
+        <Item
+          className={styles.item}
+          model={{
+            type: 'diamond',
+            size: [80, 80],
+            label: 'diamond',
+          }}
+        >
+          <img
+            src="https://gw.alicdn.com/tfs/TB1EB9VnNz1gK0jSZSgXXavwpXa-178-184.png"
+            width="89"
+            height="92"
+            draggable={false}
+          />
+        </Item>
+        <Item
+          className={styles.item}
+          model={{
+            type: 'triangle',
+            size: [30, 30],
+            label: 'triangle',
+          }}
+        >
+          <img
+            src="https://gw.alicdn.com/tfs/TB12sC2nKH2gK0jSZJnXXaT1FXa-126-156.png"
+            width="63"
+            height="78"
+            draggable={false}
+          />
+        </Item>
+      </ItemPanel>
+      <Flow className={styles.graph} data={data} />
+    </GGEditor>
   );
 }
 
